a167f14a5c87548f4eb8b8e8549b88c5d9b78c2e
opilane <opilane@HP-CZC6297B9D>
2020-09-03 10:13:41 +0300

    Работа с сфетофор, использование функции Frame

--- App_Filatov/App_Filatov/App.xaml.cs
+++ App_Filatov/App_Filatov/App.xaml.cs
@@ -10,7 +10,7 @@ namespace App_Filatov
         {
             InitializeComponent();
 
-            MainPage = new StackPage();
+            MainPage = new Valgusfoor2();
         }
 
 
---
+++ App_Filatov/App_Filatov/Valgusfoor.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_Filatov
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Valgusfoor : ContentPage
+    {
+        public Valgusfoor()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+
+        private void on_Clicked(object sender, EventArgs e)
+        {
+            int c=rnd.Next(1,4);
+            if (c == 1)
+            {
+                pun.BackgroundColor = Color.FromRgb(255, 0, 0);
+                kol.BackgroundColor = Color.Gray;
+                roh.BackgroundColor = Color.Gray;
+
+            }
+            else if(c == 2)
+            {
+                pun.BackgroundColor = Color.FromRgb(255, 0, 0);
+                kol.BackgroundColor = Color.Gray;
+                roh.BackgroundColor = Color.Gray;
+            
+            }
+            else if (c == 3)
+            {
+                pun.BackgroundColor = Color.FromRgb(255, 0, 0);
+                kol.BackgroundColor = Color.Gray;
+                roh.BackgroundColor = Color.Gray;
+            }
+
+
+        }
+
+        private void off_Clicked(object sender, EventArgs e)
+        {
+            pun.BackgroundColor = Color.FromRgb(100, 100, 100);
+            kol.BackgroundColor = Color.Gray;
+            roh.BackgroundColor = Color.FromHex("#aaaaaa");
+        }
+
+    }
+}
