b6abb8f7ed1cbcf69d997870f04f493fc57ca64e
opilane <opilane@HP-CZC6297B9D>
2020-09-09 15:42:36 +0300

    Добавление нескольких функций в светафор

--- App_Filatov/App_Filatov/Valgusfoor2.xaml.cs
+++ App_Filatov/App_Filatov/Valgusfoor2.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
@@ -12,6 +13,13 @@ namespace App_Filatov
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Valgusfoor2 : ContentPage
     {
+        Label punane, kollane, roheline;
+        Frame pun, kol, roh;
+        Button sisse, valja;
+        bool sisse_valja;
+        private int clicked1;
+        private object rohaline;
+
         public Valgusfoor2()
         {
             //InitializeComponent();
@@ -21,7 +29,7 @@ namespace App_Filatov
                 TextColor = Color.Red,
                 FontSize = 18
             };
-            Frame rframe = new Frame()
+            Frame punframe = new Frame()
             {
                 BackgroundColor = Color.Gray,
                 Content = red,
@@ -36,7 +44,7 @@ namespace App_Filatov
                 FontSize = 18
 
             };
-            Frame yframe = new Frame()
+            Frame kolframe = new Frame()
             {
                 BackgroundColor = Color.Gray,
                 Content = yellow,
@@ -51,7 +59,7 @@ namespace App_Filatov
                 FontSize = 18
 
             };
-            Frame gframe = new Frame()
+            Frame rohframe = new Frame()
             {
                 BackgroundColor = Color.Gray,
                 Content = green,
@@ -75,11 +83,64 @@ namespace App_Filatov
             };
             StackLayout stackLayout = new StackLayout()
             {
-                Children = { rframe, yframe, gframe, stackLayout2 }
+                Children = { punframe, kolframe, rohframe, stackLayout2 }
             };
             stackLayout2.Orientation = StackOrientation.Horizontal;
             stackLayout2.Margin = new Thickness(90, 0, 0, 0);
+
+            bt1.Clicked += bt1_Clicked;
+            bt2.Clicked += bt2_Clicked;
+
             Content = stackLayout;
+             
+            var tap = new TapGestureRecognizer();
+            tap.Tapped += Tap_Tapped;
+            punframe.GestureRecognizers.Add(tap);
         }
+
+        private async void bt1_Clicked(object sender, EventArgs e)
+        {
+           pun.BackgroundColor = Color.Red;
+           pun.BorderColor = Color.WhiteSmoke;
+           sisse_valja = false;
+           for (int i = 0; i < 100; i++)
+           {
+               punane.FontSize++;
+               await Task.Run(() => Thread.Sleep(1000));//Пауза в 1 сек
+           }
+            
+        }
+        private async void bt2_Clicked(object sender, EventArgs e)
+        {
+           pun.BackgroundColor = Color.Gray;
+           pun.BorderColor = Color.WhiteSmoke;
+           sisse_valja = true;
+            for (int i = 0; i <100; i++)
+            {
+                punane.FontSize++; 
+                await Task.Run(() => Thread.Sleep(1000));
+            }
+        } 
+        private void Tap_Tapped(object sender, EventArgs e)
+        {
+            Frame fr = sender as Frame;
+            if (fr == pun)
+            {
+                if (clicked1 == 0) {pun.Text = "Seisa ja oota!!!";}
+                else if (fr == pun) {punane.Text = "Punane";}
+            }
+            else if (fr == kol)
+            {
+                if (clicked1 == 0) {kol.Text = "Pole palju jäänud";}
+                else if (fr == kol) {kollane.Text = "Kollane";}
+            }
+            else if (fr == roh)
+            {
+                if (clicked1 == 0) { roh.Text = "Minne";}
+                else if (fr == roh) {rohaline.Text = "Rohaline";}
+            }
+
+        }
+
     }
 }
